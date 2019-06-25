726a0af364027cd4b7e2ef936c4c54a576e5083e
Mukul <chetanmukul19@gmail.com>
2019-06-25 18:06:08 +0530

    program

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 docker pull mukul005/omdb-api:API-Container
-docker run -td mukul005/omdb-api:API-Container
-docker cp python-api-program.py API-Container:/var/tmp/python-api-program.py
-docker exec -it API-Container bash 
+docker run --name OMDB-API-CONTAINER -td mukul005/omdb-api:API-Container
+docker cp python-api-program.py OMDB-API-CONTAINER:/var/tmp/python-api-program.py
+docker exec -it OMDB-API-CONTAINER bash 
