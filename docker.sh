b41b8e2978668984230408e827a50ebe759243d1
mukul005 <52038934+mukul005@users.noreply.github.com>
2019-06-26 15:58:41 +0530

    docker code

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 docker pull mukul005/omdb-api:API-Container
-docker run --name OMDB-API-CONTAINER -td mukul005/omdb-api:API-Container
-docker cp python-api-program.py OMDB-API-CONTAINER:/var/tmp/python-api-program.py
-docker exec -it OMDB-API-CONTAINER bash 
+docker run --name Movie-CONTAINER -td mukul005/omdb-api:API-Container
+docker cp python-api-program.py Movie-CONTAINER:/var/tmp/python-api-program.py
+docker exec -it Movie-CONTAINER bash 
