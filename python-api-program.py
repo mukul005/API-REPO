f0eed668c19c8fb1c5f29ae5272d8b4954995887
mukul005 <52038934+mukul005@users.noreply.github.com>
2019-08-14 15:46:40 +0530

    Update python-api-program.py

--- python-api-program.py
+++ python-api-program.py
@@ -1,22 +1,53 @@
 import requests
 import sys
-def MovieTitle():
-     movie_name=sys.argv[1:]
-     movie_url=f"http://www.omdbapi.com/?apikey=88deb2b&t={movie_name}"
-     return movie_url
+import urllib.request
+from goto import with_goto
 
+# Check if URL is responding or not
 
+def CHECK_URL(URL):
+    return urllib.request.urlopen(URL).getcode()
+
+@with_goto
 def main():
-    movie_url=MovieTitle()
-    data = requests.get(url = movie_url)
-    data_json = data.json()
+
+# Taking details
+
+    movie_name=sys.argv[1:]
+    URL = 'http://www.omdbapi.com'
+    api_key='88deb2b'
+    PARAMS = {'t':movie_name, 'r': 'json', 'apikey': api_key}
+
+# Checking if URL is up or not
     try:
+        if CHECK_URL(URL) == 200:
+           print("URL is working fine")
+
+    except:
+        print("URL is not working")
+        goto .END
+
+# Working on extracting information
+
+    data_json = requests.get(url = URL, params=PARAMS).json()
+
+
+    if 'Invalid API key!' in data_json.values():
+        print("Invalid API key has been provided, please use correct key")
+        goto .END
+
+
+    try:
+
         Ratings=data_json["Ratings"]
+
         try:
+
             for i in Ratings:
                 if 'Rotten Tomatoes' in i['Source']:
                     rotten=i
             Percent=rotten['Value']
+
 
             if int(Percent[:len(Percent)-1]) <30:
                 print("This movie has rating below 30% on Rotten tomatoes, can be ignored")
@@ -24,11 +55,14 @@ def main():
             else:
                 print("This movie has rating above 30% on Rotten tomatoes and could be watched atleast once.")
                 print("Rotten Tomatoes Rating :  ",rotten['Value'])
+
         except:
             print("No Rotten Tomatoes rating found")
 
     except:
         print("There is no such movie, try again")
+
+    label .END
 
 if __name__== "__main__":
         main()
